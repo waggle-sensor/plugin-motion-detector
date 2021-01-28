7c6423c47524a0176cc81e429bd47dcdd03f004a
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-01-28 09:08:08 -0600

    renamed arg

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from capture import create_video_capture_queue
 from detectors import EMAObjectDetector, BGSubObjectDetector
 
 def main():
-    parser = argparse.ArgumentParser(description="This program shows how to use background subtraction methods provided by \
-                                                OpenCV. You can process both videos and images.")
-    parser.add_argument("--dev", action="store_true", help="use local webcam and display for development")
+    parser = argparse.ArgumentParser(description="This program uses simple motion detection and background subtraction for object detection.")
+    parser.add_argument("--display", action="store_true", help="display object detection preview")
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -21,6 +20,7 @@ def main():
     # detector = BGSubObjectDetector(cv2.createBackgroundSubtractorMOG2())
     # detector = BGSubObjectDetector(cv2.createBackgroundSubtractorKNN())
     detector = EMAObjectDetector(0.7)
+
     frames = create_video_capture_queue("test.mp4")
 
     try:
@@ -29,7 +29,7 @@ def main():
             objects = detector.apply(frame)
             for x, y, w, h in objects:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            if args.dev:
+            if args.display:
                 cv2.imshow("Preview", frame)
                 keyboard = cv2.waitKey(1) & 0xFF
                 if keyboard == ord("q") or keyboard == 27:
